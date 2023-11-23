71ffd88769ae01aa9e48b1966cbae6b78d2c645a
2190031 <2190031@ipisa.edu.do>
2023-11-23 15:32:31 -0400

    Almacenar datos de vendedores y zonas, ramificacion de menues (mejorar), funciones de mostrar datos

--- proyecto_algoritmia/main.c
+++ proyecto_algoritmia/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <windows.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define MAX_PRODUCT_NAME_LENGTH 25
 
@@ -42,48 +43,94 @@ void font_colour(short int colour){
     }
 }
 
+/*MENUS*/
 void show_main_menu(){
     font_colour(blue);
     printf("-- MENU --\n");
     font_colour(original);
-    printf("(1) Insertar\n(2)...\n> ");
+    printf("(1) Insertar\n(2) Mostrar\n(3) Eliminar\n> ");
 }
 
-void print_products(
-    short int idProd[5],
-    char *prodName[5],
-    char *prodType[5],
-    float prodPurchPrice[5],
-    float prodSellPrice[5],
-    float prodRev[5],
-    short int prodAmountSold[5]
-    ) {
+void show_insert_menu(){
+    font_colour(blue);
+    printf("\t-- INSERTAR --\n");
+    font_colour(original);
+    printf("\t(1) Producto\n\t(2) Vendedor\n\t(3) Zona\n> ");
+}
+
+void show_view_menu(){
+    font_colour(blue);
+    printf("\t-- MOSTRAR --\n");
+    font_colour(original);
+    printf("\t(1) Producto\n\t(2) Vendedor\n\t(3) Zona\n> ");
+}
+
+void show_delete_menu(){
+    font_colour(blue);
+    printf("\t-- ELIMINAR --\n");
+    font_colour(original);
+    printf("\t(1) Producto\n\t(2) Vendedor\n\t(3) Zona\n> ");
+}
+
+/*MOSTRAR*/
+void print_products(short int id[5], char *name[5], char *type[5], float purchPrice[5], float sellPrice[5], float revenue[5], short int amountSold[5]) {
     short int i;
     font_colour(cyan);
     printf("\n{\n\t\"Productos\": [\n");
     for (i = 0; i < 5; i++) {
+        printf("\t\t{\n\t\t\t\"ID\": %hd,\n", id[i]);
+        printf("\t\t\tNombre: \"%s\",\n", name[i]);
+        printf("\t\t\tTipo: \"%s\",\n", type[i]);
+        printf("\t\t\tPrecio_compra: %.2f,\n", purchPrice[i]);
+        printf("\t\t\tPrecio_venta: %.2f,\n", sellPrice[i]);
+        printf("\t\t\tGanancia: %.2f,\n", revenue[i]);
         if (i<4) {
-            printf("\t\t{\n\t\t\t\"ID\": %hd,\n", idProd[i]);
-            printf("\t\t\tNombre: \"%s\",\n", prodName[i]);
-            printf("\t\t\tTipo: \"%s\",\n", prodType[i]);
-            printf("\t\t\tPrecio_compra: %.2f,\n", prodPurchPrice[i]);
-            printf("\t\t\tPrecio_venta: %.2f,\n", prodSellPrice[i]);
-            printf("\t\t\tGanancia: %.2f,\n", prodRev[i]);
-            printf("\t\t\tCantidad_vendida: %hd\n\t\t},\n", prodAmountSold[i]);
+            printf("\t\t\tCantidad_vendida: %hd\n\t\t},\n", amountSold[i]);
         } else if (i==4) {
-            printf("\t\t{\n\t\t\t\"ID\": %hd,\n", idProd[i]);
-            printf("\t\t\tNombre: \"%s\",\n", prodName[i]);
-            printf("\t\t\tTipo: \"%s\",\n", prodType[i]);
-            printf("\t\t\tPrecio_compra: %.2f,\n", prodPurchPrice[i]);
-            printf("\t\t\tPrecio_venta: %.2f,\n", prodSellPrice[i]);
-            printf("\t\t\tGanancia: %.2f,\n", prodRev[i]);
-            printf("\t\t\tCantidad_vendida: %hd\n\t\t}\n", prodAmountSold[i]);
+            printf("\t\t\tCantidad_vendida: %hd\n\t\t}\n", amountSold[i]);
         }
     }
     printf("\t]\n}\n");
     font_colour(original);
 }
 
+void print_sellers(short int id[5], char *name[5], short int amountSold[5], float revenue[5]) {
+    short int i;
+    font_colour(cyan);
+    printf("\n{\n\t\"Vendedores\": [\n");
+    for (i = 0; i < 10; i++) {
+        printf("\t\t{\n\t\t\t\"ID\": %hd,\n", id[i]);
+        printf("\t\t\tNombre: \"%s\",\n", name[i]);
+        printf("\t\t\tCantidad_ventas: %hd,\n", amountSold[i]);
+        if (i<9) {
+            printf("\t\t\tGanancias: %.2f\n\t\t},\n", revenue[i]);
+        } else if (i==9) {
+            printf("\t\t\tGanancias: %.2f\n\t\t}\n", revenue[i]);
+        }
+    }
+    printf("\t]\n}\n");
+    font_colour(original);
+}
+
+void print_zones(short int id[5], char *name[5], short int amountSold[5], float revenue[5]) {
+    short int i;
+    font_colour(cyan);
+    printf("\n{\n\t\"Zonas\": [\n");
+    for (i = 0; i < 5; i++) {
+        printf("\t\t{\n\t\t\t\"ID\": %hd,\n", id[i]);
+        printf("\t\t\tNombre: \"%s\",\n", name[i]);
+        printf("\t\t\tCantidad_ventas: %hd,\n", amountSold[i]);
+        if (i<4) {
+            printf("\t\t\tGanancias: %.2f\n\t\t},\n", revenue[i]);
+        } else if (i==4) {
+            printf("\t\t\tGanancias: %.2f\n\t\t}\n", revenue[i]);
+        }
+    }
+    printf("\t]\n}\n");
+    font_colour(original);
+}
+
+/*INSERTAR*/
 void enterProductData(short int i, char *name[5], char *type[5], float purchPrice[5], float sellPrice[5], float revenue[5]) {
     name[i] = (char *)malloc(MAX_PRODUCT_NAME_LENGTH + 1);
     type[i] = (char *)malloc(MAX_PRODUCT_NAME_LENGTH + 1);
@@ -95,7 +142,7 @@ void enterProductData(short int i, char *name[5], char *type[5], float purchPric
         font_colour(original);
         exit(EXIT_FAILURE);
     }
-    
+
     font_colour(green);
     printf("\nProducto no. %hd:\n", i + 1);
     font_colour(original);
@@ -114,7 +161,7 @@ void enterProductData(short int i, char *name[5], char *type[5], float purchPric
     printf("\tPrecio_compra > ");
     font_colour(original);
     scanf("%f", &purchPrice[i]);
-    
+
     font_colour(yellow);
     printf("\tPrecio_venta  > ");
     font_colour(original);
@@ -123,30 +170,131 @@ void enterProductData(short int i, char *name[5], char *type[5], float purchPric
     revenue[i] = sellPrice[i] - purchPrice[i];
 }
 
-int main() {
-    short int id_seller [10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    short int idProd [5] = {1, 2, 3, 4, 5};
-    short int id_zone [5] = {1, 2, 3, 4, 5};
+void enterSellerData(short int i, char *name[5]) {
+    name[i] = (char *)malloc(MAX_PRODUCT_NAME_LENGTH + 1);
 
-    char *sell_name [10], *prodName [5], *zone_name [5], *prodType [5];
-    short int sell_amount_sold [10], prodAmountSold [5] = {0,0,0,0,0}, zone_amount_sold [5];
+    if (name[i] == NULL) {
+        // Handle allocation failure
+        font_colour(red);
+        fprintf(stderr, "Memory allocation failed.\n");
+        font_colour(original);
+        exit(EXIT_FAILURE);
+    }
+
+    font_colour(green);
+    printf("\nVendedor no. %hd:\n", i + 1);
+    font_colour(original);
+
+    font_colour(yellow);
+    printf("\tNombre        > ");
+    font_colour(original);
+    scanf("%s", name[i]);
+}
+
+void enterZoneData(short int i, char *name[5]) {
+    name[i] = (char *)malloc(MAX_PRODUCT_NAME_LENGTH + 1);
+
+    if (name[i] == NULL) {
+        // Handle allocation failure
+        font_colour(red);
+        fprintf(stderr, "Memory allocation failed.\n");
+        font_colour(original);
+        exit(EXIT_FAILURE);
+    }
+
+    font_colour(green);
+    printf("\nZona no. %hd:\n", i + 1);
+    font_colour(original);
+
+    font_colour(yellow);
+    printf("\tNombre        > ");
+    font_colour(original);
+    scanf("%s", name[i]);
+}
+
+/*ELIMINAR*/
+int main() {
+    bool repeat = true;
+
+    short int idSeller [10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    short int idProd [5] = {1, 2, 3, 4, 5};
+    short int idZone [5] = {1, 2, 3, 4, 5};
+
+    char *sellerName [10], *prodName [5], *zoneName [5], *prodType [5];
+
+    short int sellerAmountSold [10] = {0,0,0,0,0,0,0,0,0,0};
+    short int prodAmountSold [5] = {0,0,0,0,0};
+    short int zoneAmountSold [5] = {0,0,0,0,0};
+
     float prodPurchPrice [5], prodSellPrice [5];
-    float sell_rev [10], prodRev [5], zone_rev [5];
+    float sellerRevenue [10] = {0,0,0,0,0,0,0,0,0,0}, prodRevenue [5], zoneRevenue [5] = {0,0,0,0,0};
 
     int short sales [10][2];
 
     short int opc, i;
-    show_main_menu();
-    scanf("%hd", &opc);
-    switch (opc) {
-    case 1:
-        for (i = 0; i < 5; i++) {
-            enterProductData(i, prodName, prodType, prodPurchPrice, prodSellPrice, prodRev);
+    while (repeat) {
+        show_main_menu();
+        scanf("%hd", &opc);
+        switch (opc) {
+        case 1:
+            show_insert_menu();
+            scanf("%hd", &opc);
+            switch (opc) {
+            case 1:
+                for (i = 0; i < 5; i++) {
+                    enterProductData(i, prodName, prodType, prodPurchPrice, prodSellPrice, prodRevenue);
+                }
+                // print_products(idProd, prodName, prodType, prodPurchPrice, prodSellPrice, prodRevenue, prodAmountSold);
+                break;
+            case 2:
+                for (i = 0; i < 10; i++) {
+                    enterSellerData(i, sellerName);
+                }
+                // print_sellers(idSeller, sellerName, sellerAmountSold, sellerRevenue);
+                break;
+            case 3:
+                for (i = 0; i < 5; i++) {
+                    enterZoneData(i, zoneName);
+                }
+                // print_zones(idZone, zoneName, zoneAmountSold, zoneRevenue);
+                break;
+            default:
+                break;
+            }
+        case 2:
+            show_view_menu();
+            scanf("%hd", &opc);
+            switch (opc) {
+            case 1:
+                print_products(idProd, prodName, prodType, prodPurchPrice, prodSellPrice, prodRevenue, prodAmountSold);
+                break;
+            case 2:
+                print_sellers(idSeller, sellerName, sellerAmountSold, sellerRevenue);
+                break;
+            case 3:
+                print_zones(idZone, zoneName, zoneAmountSold, zoneRevenue);
+                break;
+            default:
+                break;
+            }
+        case 3:
+        show_delete_menu();
+            scanf("%hd", &opc);
+            switch (opc) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                break;
+            }
+            break;
+        default:
+            repeat=false;
+            break;
         }
-        print_products(idProd, prodName, prodType, prodPurchPrice, prodSellPrice, prodRev, prodAmountSold);
-        break;
-    default:
-        break;
     }
     return 0;
 }
